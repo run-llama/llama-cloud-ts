2d6f3d0112ef67a46d8a90e0eeff333d8c7d7431
Logan Markewich <logan.markewich@live.com>
2025-12-23 15:39:19 -0600

    fix tests

--- tests/api-resources/parsing.test.ts
+++ tests/api-resources/parsing.test.ts
@@ -204,30 +204,4 @@ describe('resource parsing', () => {
       ),
     ).rejects.toThrow(LlamaCloud.NotFoundError);
   });
-
-  // Prism tests are disabled
-  test.skip('uploadFile', async () => {
-    const responsePromise = client.parsing.uploadFile();
-    const rawResponse = await responsePromise.asResponse();
-    expect(rawResponse).toBeInstanceOf(Response);
-    const response = await responsePromise;
-    expect(response).not.toBeInstanceOf(Response);
-    const dataAndResponse = await responsePromise.withResponse();
-    expect(dataAndResponse.data).toBe(response);
-    expect(dataAndResponse.response).toBe(rawResponse);
-  });
-
-  // Prism tests are disabled
-  test.skip('uploadFile: request options and params are passed correctly', async () => {
-    // ensure the request options are being passed correctly by passing an invalid HTTP method in order to cause an error
-    await expect(
-      client.parsing.uploadFile(
-        {
-          organization_id: '182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e',
-          project_id: '182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e',
-        },
-        { path: '/_stainless_unknown_path' },
-      ),
-    ).rejects.toThrow(LlamaCloud.NotFoundError);
-  });
 });
